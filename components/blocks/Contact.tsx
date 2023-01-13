e50b3e884726d3b62c43083e83b01b05353f8335
nazar-si <nazari61@yandex.ru>
2023-01-13 14:24:08 +0300

    Added contact form

--- components/blocks/Contact.tsx
+++ components/blocks/Contact.tsx
@@ -1,8 +1,40 @@
 import style from "./contact.module.css";
 import React from 'react'
+import common from "./common.module.css";
+import { Building, MapPin, Mail, } from "tabler-icons-react";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className={style.wrapper}>
+      <div className={common.title}>
+        Contact
+      </div>
+      <div className={style.content}>
+        <form className={style.form}>
+          <input type="text" name="" id="" placeholder="name"/>
+          <input type="text" name="" id="" placeholder="Phone / Email"/>
+          <textarea name="message" id="" rows={10} ></textarea>
+        </form>
+        <div className={style.info}>
+          <div className={style.row}>
+            <div className={style.icon}><MapPin/></div>
+            <div className={style.text}>Russia, Moscow</div>
+          </div>
+          <div className={style.row}>
+            <div className={style.icon}><Building/></div>
+            <div className={style.text}><a href="https://github.com/EntropyConcept">Entropy Concept</a></div>
+          </div>
+          <div className={style.row}>
+            <div className={style.icon}><Mail/></div>
+            <div className={style.text}><a href="mailto:nisimchuk@edu.hse.ru">nisimchuk@edu.hse.ru</a></div>
+          </div>
+          <div className={style.row}>
+            <div className={style.icon}><Mail/></div>
+            <div className={style.text}><a href="mailto:technotche@gmail.com">technotche@gmail.com</a></div>
+          </div>
+          <hr />
+        </div>
+      </div>
+    </div>
   )
 }
---
+++ components/blocks/Media.tsx
@@ -0,0 +1,8 @@
+import style from "./media.module.css"
+import React from 'react'
+
+export default function Media() {
+  return (
+    <div>Media</div>
+  )
+}
