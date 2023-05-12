d32c8c5abcd7b6b5735814a8c32f8967493b9274
nazar-si <nazari61@yandex.ru>
2023-05-12 13:27:40 +0300

    Fixed triangle text and added section to roadmap

--- components/blocks/Roadmap.tsx
+++ components/blocks/Roadmap.tsx
@@ -5,20 +5,20 @@ import { GitCommit, GitFork, RotateDot } from "tabler-icons-react"
 
 const Entries = [
     {
-        title: "Crafting a design system for a multiplanetary future",
-        description: "Most companies try to stay ahead of the curve when it comes to visual design, but for Planetaria we needed to create a brand that would still inspire us 100 years from now when humanity has spread across our entire solar system.",
-        date: new Date("2022-09-05")
+        title: "Optimize and Organize Hack",
+        description: "For this hackathon I was building dashboard for monitoring of the condition of construction facilities. It was very tedious process and despite the fact that I haven't gotten any winning place I was very glad for experience. This project showed me how far I've gotten with React development, when it became tool that I can fully understand and control.",
+        date: new Date("2023-04-11")
     },
-    {
-        title: "Introducing Animaginary: High performance web animations",
-        description: "When you’re building a website for a company as ambitious as Planetaria, you need to make an impression. I wanted people to visit our website and see animations that looked more realistic than reality itself.",
-        date: new Date("2022-09-02")
-    },
-    {
-        title: "Rewriting the cosmOS kernel in Rust",
-        description: "When we released the first version of cosmOS last year, it was written in Go. Go is a wonderful programming language with a lot of benefits, but it’s been a while since I’ve seen an article on the front page of Hacker News about rewriting some important tool in Go and I see articles on there about rewriting things in Rust every single week.",
-        date: new Date("2022-08-22")
-    }
+    // {
+    //     title: "Introducing Animaginary: High performance web animations",
+    //     description: "When you're building a website for a company as ambitious as Planetaria, you need to make an impression. I wanted people to visit our website and see animations that looked more realistic than reality itself.",
+    //     date: new Date("2022-09-02")
+    // },
+    // {
+    //     title: "Rewriting the cosmOS kernel in Rust",
+    //     description: "When we released the first version of cosmOS last year, it was written in Go. Go is a wonderful programming language with a lot of benefits, but it’s been a while since I’ve seen an article on the front page of Hacker News about rewriting some important tool in Go and I see articles on there about rewriting things in Rust every single week.",
+    //     date: new Date("2022-08-22")
+    // }
 ]
 
 const formatter = new Intl.DateTimeFormat("en", {month: "long", year: "numeric", day: "numeric"})
@@ -31,7 +31,7 @@ export default function roadmap() {
         </div>
         <div className={style.container}>
             <h2 className={common.title}>Roadmap</h2>
-            <h2 className={common.subtitle}>Things I've experienced so far. <span className="text-indigo-400">This section is under development and uses placeholder text.</span></h2>
+            <h2 className={common.subtitle}>Things I've experienced so far. <span className="text-indigo-400">This section is under development.</span></h2>
             <br />
             {Entries.map((e, i) => <div className={style.entry} key={i}>
                 <div className={style.road}>
