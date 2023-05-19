d4b02be6d330acfe3f118bbaff36112558675d44
nazar-si <nazari61@yandex.ru>
2023-05-19 17:01:12 +0300

    ci: added github lighthouse workflow

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ci: {
+      collect: {
+        url: ['https://nazarsi.com'],
+      },
+      upload: {
+        target: 'temporary-public-storage',
+      },
+    },
+  };
